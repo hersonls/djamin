d34d5f0c6940c74a2f964e2a1bb0461aa0ac740c
Hersonls <hersonls@gmail.com>
2014-07-31 15:04:34 -0300

    Making django admin a better place :D

---
+++ README.md
@@ -0,0 +1,22 @@
+Djamin
+=====
+
+Djamin a new and clean styles for Django admin based in Google projects styles.
+
+Quick start
+-----------
+
+1. Add "djamin" to your INSTALLED_APPS setting, before django admin app 
+   like this:
+
+```python
+   INSTALLED_APPS = (
+       'djamin',
+       'django.contrib.admin',
+       ...
+       
+   )
+```
+
+2. Start the development server and visit http://127.0.0.1:8000/admin/ and 
+   anjoy.
---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='djamin',
+    version='0.1',
+    packages=['djamin'],
+    include_package_data=True,
+    license='MIT License',
+    description='A new style for Django admin.',
+    long_description=README,
+    url='https://github.com/hersonls/djamin',
+    author='Herson Leite',
+    author_email='hersonls@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ]
+)
